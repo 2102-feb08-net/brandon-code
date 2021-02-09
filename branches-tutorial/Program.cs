6c39effb7a8f100779adce473eb2322061ebde8b
Exsolutus <exsolutus.dev@gmail.com>
2021-02-09 16:51:19 -0500

    Various project directories based on the tutorials found here: https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/intro-to-csharp/numbers-in-csharp-local

---
+++ branches-tutorial/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace BranchesAndLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ExploreIf();
+
+            //ExploreLoop();
+
+            int sum = 0;
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+
+        static void ExploreIf()
+        {
+            int a = 5;
+            int b = 3;
+            if (a + b > 10)
+            {
+                Console.WriteLine("The answer is greater than 10");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+            }
+
+            int c = 4;
+            if ((a + b + c > 10) && (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not equal to the second");
+            }
+
+            if ((a + b + c > 10) || (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("Or the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not equal to the second");
+            }
+        }
+
+        static void ExploreLoop()
+        {
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            }
+
+            counter = 0;
+            do
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            } while (counter < 10);
+
+            for (int index = 5; index < 21; index++)
+            {
+                Console.WriteLine($"Hello World! The index is {index}");
+            }
+
+            for (int row = 1; row < 11; row++)
+            {
+                for (char column = 'a'; column < 'k'; column++)
+                {
+                    Console.WriteLine($"The cell is ({row}, {column})");
+                }
+            }
+        }
+    }
+}
